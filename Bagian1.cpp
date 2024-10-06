013e33ba66e4acbb4213f611dab60e89f5ea1a13
Nicholasdeva <nicholasdevayaman1211@gmail.com>
2024-10-06 21:02:38 +0700

    Bagian1.cpp

--- Bagian1.cpp
+++ Bagian1.cpp
@@ -60,3 +60,29 @@ void tampilkanMenu()
     cout<<"0. Keluar/Total Harga"<<endl;
     cout<<"========================"<<endl;
 }
+
+void pilihMenu(int pilihan, int &totalMakanan, int &totalMinuman)
+{
+    if (pilihan >= 1 && pilihan <= 5)
+    {
+        totalMakanan += hargaMakanan[pilihan - 1];
+        cout<<"Anda memilih makanan."<<endl;
+    }
+    else if (pilihan >= 6 && pilihan <= 10)
+    {
+        totalMinuman += hargaMinuman[pilihan - 6];
+        cout << "Anda memilih minuman."<<endl;
+    }
+}
+
+void tampilkanTotalPembelian(int totalMakanan, int totalMinuman)
+{
+    int total = totalMakanan + totalMinuman;
+
+    cout<<"=========== Total Pembelian ============"<<endl;
+    cout<<"Total makanan   : " << formatHarga(totalMakanan) <<endl;
+    cout<<"Total minuman   : " << formatHarga(totalMinuman) <<endl;
+    cout<<"========================================"<<endl;
+    cout<<"Total keseluruhan : " << formatHarga(total) <<endl;
+    cout<<"=====Terima kasih telah berbelanja!====="<<endl;
+}
